cc45ee1d0d9e7f1fd40aa62cc1c1488df5b74cf8
yy319 <yy319@duke.edu>
2022-03-15 19:14:26 -0400

    update3

---
+++ serve_hw3.bat
@@ -0,0 +1,5 @@
+cd C:\Users\yy319\PycharmProjects\homework_3-1
+git pull https://%TESTAPP_GIT_PAT%@github.com/hushusky/homework_3-1.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
+
